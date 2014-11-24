00e91e69de1c7c2c190f51dee1e21315ac48f0ce
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-11-24 17:43:59 -0500

    Adds isDSAffiliate()

--- src/MB_Toolbox.php
+++ src/MB_Toolbox.php
@@ -25,5 +25,38 @@ class MB_Toolbox
     $this->statHat = new StatHat($settings['stathat_ez_key'], 'MB_Toolbox:');
     $this->statHat->setIsProduction(FALSE);
   }
- 
+
+  /**
+   * Test if country code has a  DoSomething affiliate.
+   *
+   * Follow country code convention defined in:
+   * http://dev.maxmind.com/geoip/legacy/codes/iso3166/
+   *
+   * @param string $targetCountyCode
+   *   Details about the user to create Drupal account for.
+   *
+   * @return boolean $foundAffiliate
+   *   Test if supplied country code is a DoSomething affiliate country.
+   */
+  private function isDSAffiliate($targetCountyCode) {
+
+    $foundAffiliate = FALSE;
+
+    $affiliates = array(
+      'US', // United States
+      'GB', // United Kingdom
+      'CA', // Canada
+      'ID', // Indonesia
+      'BW', // Botswana
+      'KE', // Kenya
+      'GH', // Ghana
+      'NG', // Nigeria
+      'CD', // Congo, The Democratic Republic of the"
+    );
+
+    if (in_array($targetCountyCode, $affiliates)) {
+      $foundAffiliate = TRUE;
+    }
+
+  return $foundAffiliate;
 }
