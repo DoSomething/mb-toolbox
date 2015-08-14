557190c8314f727d1e7aa2527617190774f2da43
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-08-13 20:08:21 -0400

    Move cURL GET methods from MB_Toolbox class

--- src/MB_Toolbox_cURL.php
+++ src/MB_Toolbox_cURL.php
@@ -13,11 +13,11 @@ class MB_Toolbox_cURL
   const TIMEOUT = 0;
 
   /**
-   * Service settings
+   * Singleton instance of MB_Configuration application settings and service objects
    *
-   * @var array
+   * @var object
    */
-  private $settings;
+  protected $mbConfig;
 
   /**
    * Setting from external service to track activity - StatHat.
@@ -41,8 +41,8 @@ class MB_Toolbox_cURL
    *
    * @return object
    */
-  public function __construct($settings) {
-    $this->settings = $settings;
+  public function __construct() {
+
     $this->auth = NULL;
 
     // Check for cURL on server
@@ -50,8 +50,80 @@ class MB_Toolbox_cURL
       throw new Exception("Error - PHP cURL extension is not enabled on the server. cURL is required by many of the methods in the mb-toolbox library.");
     }
 
-    $this->statHat = new StatHat($settings['stathat_ez_key'], 'MB_Toolbox:');
-    $this->statHat->setIsProduction(TRUE);
+    $this->mbConfig = MB_Configuration::getInstance();
+    $this->statHat = $this->mbConfig->getProperty('statHat');
+  }
+
+  /**
+   * cURL GETs
+   *
+   * @param string $curlUrl
+   *  The URL to GET from. Include domain and path.
+   * @param boolean $isAuth
+   *  Optional flag to keep track of the current authencation state.
+   *
+   * @return boolean $results
+   *  Response from cURL GET request.
+   *
+   * @return object $result
+   *   The results returned from the cURL call.
+   *   - [0]: Results in json format
+   *   - [1]: Status code
+   */
+  public function curlGET($curlUrl, $isAuth = FALSE) {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $curlUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // Only add token and cookie values to header when values are available and
+    // the curlPOSTauth() method is making the POST request.
+    if (isset($this->auth->token) && $isAuth) {
+      curl_setopt($ch, CURLOPT_HTTPHEADER,
+        array(
+          'Content-type: application/json',
+          'Accept: application/json',
+          'X-CSRF-Token: ' . $this->auth->token,
+          'Cookie: ' . $this->auth->session_name . '=' . $this->auth->sessid
+        )
+      );
+    }
+    else {
+      curl_setopt($ch, CURLOPT_HTTPHEADER,
+        array(
+          'Content-type: application/json',
+          'Accept: application/json'
+        )
+      );
+    }
+
+    $jsonResult = curl_exec($ch);
+    $results[0] = json_decode($jsonResult);
+    $results[1] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return $results;
+  }
+
+  /**
+   * cURL GET with authentication
+   *
+   * @param string $curlUrl
+   *  The URL to GET to. Include domain and path.
+   *
+   * @return object $result
+   *   The results returned from the cURL call.
+   */
+  public function curlGETauth($curlUrl) {
+
+    // Remove authentication until POST to /api/v1/auth/login is resolved
+    if (!isset($this->auth)) {
+      $this->authenticate();
+    }
+
+    $results = $this->curlGET($curlUrl, TRUE);
+
+    return $results;
   }
 
   /**
@@ -82,6 +154,44 @@ class MB_Toolbox_cURL
     curl_close($ch);
 
     return $results;
+  }
+
+  /**
+   * Authenticate for API access
+   */
+  private function authenticate() {
+
+    if (!empty($this->mbConfig->getProperty('ds_drupal_api_username')) && !empty($this->mbConfig->getProperty('ds_drupal_api_password'))) {
+      $post = array(
+        'username' => $this->mbConfig->getProperty('ds_drupal_api_username'),
+        'password' => $this->mbConfig->getProperty('ds_drupal_api_password'),
+      );
+    }
+    else {
+      trigger_error("MB_Toolbox->authenticate() : username and/or password not defined.", E_USER_ERROR);
+      exit(0);
+    }
+
+    // @todo: Abstract into URL assembly function
+    $curlUrl = $this->mbConfig->getProperty('ds_drupal_api_host');
+    $port = $this->mbConfig->getProperty('ds_drupal_api_port');
+    if ($port > 0 && is_numeric($port)) {
+      $curlUrl .= ':' . (int) $port;
+    }
+
+    // https://www.dosomething.org/api/v1/auth/login
+    $curlUrl .= self::DRUPAL_API . '/auth/login';
+    $auth = $this->curlPOST($curlUrl, $post);
+
+    if ($auth[1] == 200) {
+      $auth = $auth[0];
+    }
+    else {
+      ECHO 'ERROR - Failed to get auth creds: ' . $curlUrl . ' with POST: ' . print_r($post, TRUE), PHP_EOL;
+      $auth = FALSE;
+    }
+
+    $this->auth = $auth;
   }
 
 }
