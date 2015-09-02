8370d81ab9b27c3a192f2017fed014351848d5fd
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-09-01 22:53:20 -0400

    Addition of MB_RabbitMQManagementAPI class

---
+++ src/MB_RabbitMQManagementAPI.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace DoSomething\MB_Toolbox;
+
+use DoSomething\MBStatTracker\StatHat;
+use RabbitMq\ManagementApi\Client as RabbitMqManagementApi;
+
+/**
+ * MBC_UserRegistration class - functionality related to the Message Broker
+ * consumer mbc-registration-email.
+ */
+class MB_RabbitMQManagementAPI
+{
+
+  /**
+   * Collection of secret connection settings.
+   * @var array
+   */
+  private $credentials;
+  
+  /**
+   * RabbitMQ Management API.
+   * @var array
+   */
+  private $rabbitManagement;
+  
+  /**
+   * Target vertual host defined in RabbitMQ server.
+   * @var string $vhost
+   */
+  private $vhost;
+  
+  /**
+   * Singleton instance of MB_Configuration application settings and service objects
+   *
+   * @var object
+   */
+  private $mbConfig;
+
+  /**
+   * Setting from external service to track activity - StatHat.
+   *
+   * @var object
+   */
+  private $statHat;
+
+  /**
+   * Constructor for MB_RabbitMQManagementAPI
+   *
+   * @param array $config
+   *   Configuration settings from mb-config.inc
+   */
+  public function __construct($conig) {
+
+    $domain = 'http://' . $conig['domain'];
+    $port = $conig['port'];
+    $vhost = $conig['vhost'];
+    $this->vhost = $vhost;
+    $password = $conig['password'];
+
+    if ($port > 0 && is_numeric($port)) {
+      $domain .= ':' . (int) $port;
+    }
+
+    $this->rabbitManagement = new RabbitMqManagementApi(
+      NULL,
+      $domain,
+      $vhost,
+      $password
+    );
+    
+    $this->mbConfig = MB_Configuration::getInstance();
+    $this->statHat = $this->mbConfig->getProperty('statHat');
+  }
+
+  /**
+   * Gather queue status numbers: ready and unacked.
+   *
+   * @param string $queueName
+   *   The name of the queue to return stats for.
+   *
+   * @return array $queueStats
+   *   The current queue ready and unacked values.
+   */
+  public function queueStatus($queueName) {
+
+    $queue = $this->rabbitManagement->queues()->get($this->vhost, $queueName);
+
+    $queueStats['ready'] = $queue['messages_ready'];
+    $queueStats['unacked'] = $queue['messages_unacknowledged'];
+
+    return $queueStats;
+  }
+
+}
